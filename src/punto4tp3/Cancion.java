7e763049410d1a43f62e31126a271043f6487777
Alexander Malveira <malveiralexander@outlook.com>
2018-05-09 20:34:18 -0300

    Se modificaron archivos
    ....
    
    Co-Authored-By: natancandia <natancandia@users.noreply.github.com>

--- src/punto4tp3/Cancion.java
+++ src/punto4tp3/Cancion.java
@@ -11,4 +11,51 @@ package punto4tp3;
  */
 public class Cancion {
     
+    private String titulo;
+    private String autor;
+    
+    
+    public Cancion(){
+        
+    }
+    
+    public Cancion(String titulo, String autor){
+        this.titulo = titulo;
+        this.autor = autor;
+    }
+
+    /**
+     * @return the titulo
+     */
+    public String getTitulo() {
+        return titulo;
+    }
+
+    /**
+     * @param titulo the titulo to set
+     */
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    /**
+     * @return the autor
+     */
+    public String getAutor() {
+        return autor;
+    }
+
+    /**
+     * @param autor the autor to set
+     */
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+    
+    
+    @Override
+    public String toString(){
+        return ("El titulo de la cancion es " + getTitulo() + ", el autor es " + getAutor());
+    }
+    
 }
--- src/punto4tp3/ListaReproduccion.java
+++ src/punto4tp3/ListaReproduccion.java
@@ -4,11 +4,59 @@
  * and open the template in the editor.
  */
 package punto4tp3;
+import java.util.Scanner;
 
 /**
  *
  * @author malve
  */
 public class ListaReproduccion {
+    
+    private Cancion[] song;
+    private int i = 1;
+    
+    public ListaReproduccion(){
+        song = new Cancion[1];
+        i++;
+    }
+    
+    public void insertar (Cancion nuevaCancion){
+        nuevaCancion = new Cancion();
+//        song[]
+    }
+    
+    
+    public void reemplazarCancion (Cancion nuevaCancion, int indice){
+    
+    
+    }
+    
+    public void removerEn (int indice){
+    
+    
+    }
+    
+    public void insertarEn (Cancion nuevaCancion, int indice){
+    
+    
+    }
+    
+    public int getCantidadCanciones (){
+        return 1;
+    }
+    
+    public int encontrarIndice (Cancion cancionRequerida){
+        return 1;
+    }
+    
+    public boolean contiene (Cancion cancionRequerida){
+        return false;
+    }
+    
+    public Cancion getCancion (int indice){
+        return null;
+    }
+
+
     
 }
